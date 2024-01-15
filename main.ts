11a38250942169c1402b5bb5e37f382047d24ff3
teampeeta <152302872+teampeeta@users.noreply.github.com>
2024-01-15 10:58:22 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+let flashes_remaining = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.lightLevel())
+})
+input.onButtonPressed(Button.B, function () {
+    flashes_remaining = 5
+})
+basic.forever(function () {
+    while (flashes_remaining > 0) {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.pause(300)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.pause(300)
+        flashes_remaining += -1
+        pins.digitalWritePin(DigitalPin.P1, 0)
+    }
+})
+basic.forever(function () {
+    if (input.lightLevel() < 40) {
+        led.plot(0, 0)
+        flashes_remaining = 5
+    } else {
+        led.unplot(0, 0)
+    }
+})
