18a6aed59e4e54e36814210cb58b7ab7c1e8b1cb
teampeeta <152302872+teampeeta@users.noreply.github.com>
2024-01-19 10:42:07 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,18 +3,13 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.lightLevel())
 })
 input.onButtonPressed(Button.B, function () {
-    flashes_remaining = 5
+	
 })
 basic.forever(function () {
-    while (flashes_remaining > 0) {
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        pins.digitalWritePin(DigitalPin.P2, 1)
-        basic.pause(300)
-        pins.digitalWritePin(DigitalPin.P1, 1)
-        pins.digitalWritePin(DigitalPin.P2, 0)
-        basic.pause(300)
-        flashes_remaining += -1
-        pins.digitalWritePin(DigitalPin.P1, 0)
+    if (flashes_remaining > 0) {
+        pins.servoWritePin(AnalogPin.P0, 180)
+    } else {
+        pins.servoWritePin(AnalogPin.P0, 90)
     }
 })
 basic.forever(function () {
@@ -23,5 +18,17 @@ basic.forever(function () {
         flashes_remaining = 5
     } else {
         led.unplot(0, 0)
+    }
+})
+basic.forever(function () {
+    while (flashes_remaining > 0) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.pause(300)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        flashes_remaining += -1
+        basic.pause(300)
+        pins.digitalWritePin(DigitalPin.P0, 0)
     }
 })
